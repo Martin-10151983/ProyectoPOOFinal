7e8937abab3aad79f974b07bf4bc683698a50cfc
A19B62290 <A19B62290@A19B62290.educacion.local>
2023-07-23 00:54:17 -0400

    Añadiendo atributos, constructores y metodos getters y setters a la clase Tienda

--- Proyecto_Final_EQUIPO-2/src/logico/Tienda.java
+++ Proyecto_Final_EQUIPO-2/src/logico/Tienda.java
@@ -1,5 +1,52 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Tienda {
+	private ArrayList<Componente> inventario;
+	private ArrayList<Cliente> clientes;
+	private ArrayList<Factura> facturas;
+	private ArrayList<Combo> combos;
+	
+	public Tienda() {
+		super();
+		this.inventario = new ArrayList<>();
+		this.clientes = new ArrayList<>();
+		this.facturas = new ArrayList<>();
+		this.combos = new ArrayList<>();
+	}
+
+	public ArrayList<Componente> getInventario() {
+		return inventario;
+	}
+
+	public void setInventario(ArrayList<Componente> inventario) {
+		this.inventario = inventario;
+	}
+
+	public ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(ArrayList<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+
+	public ArrayList<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(ArrayList<Factura> facturas) {
+		this.facturas = facturas;
+	}
+
+	public ArrayList<Combo> getCombos() {
+		return combos;
+	}
+
+	public void setCombos(ArrayList<Combo> combos) {
+		this.combos = combos;
+	}
+	
 
 }
