d450a9f1da9e669bb6c9a614074a0b2bda32590a
franc <franc@DESKTOP-CSEDFDH>
2023-07-28 15:46:48 -0400

    Modificacion de clase crear Factura

--- Proyecto_Final_EQUIPO-2/src/Visual/Crear_Factura.java
+++ Proyecto_Final_EQUIPO-2/src/Visual/Crear_Factura.java
@@ -27,6 +27,9 @@ public class Crear_Factura extends JDialog {
 	private JTextField txtF;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -54,10 +57,6 @@ public class Crear_Factura extends JDialog {
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(80, 135, 297, 84);
-		panel.add(scrollPane);
-		
 		JLabel lblNewLabel_1 = new JLabel("N\u00BA factura");
 		lblNewLabel_1.setBounds(20, 36, 63, 14);
 		panel.add(lblNewLabel_1);
@@ -65,7 +64,7 @@ public class Crear_Factura extends JDialog {
 		txtF = new JTextField();
 		txtF.setEditable(false);
 		txtF.setText("F-0001");
-		txtF.setBounds(80, 33, 116, 20);
+		txtF.setBounds(80, 33, 177, 20);
 		panel.add(txtF);
 		txtF.setColumns(10);
 		
@@ -73,33 +72,13 @@ public class Crear_Factura extends JDialog {
 		lblNewLabel_2.setBounds(20, 70, 46, 14);
 		panel.add(lblNewLabel_2);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(80, 67, 177, 20);
-		panel.add(comboBox);
-		
-		JButton btnNewButton = new JButton("Crear Cliente");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			Crear_Cliente crearCliente = new Crear_Cliente();
-			crearCliente.setModal(true);
-			crearCliente.setVisible(true);
-			}
-			
-		});
-		
-		
-		btnNewButton.setBounds(267, 66, 110, 23);
-		panel.add(btnNewButton);
-		
-		JLabel label = new JLabel("Cliente");
-		label.setBounds(20, 104, 46, 14);
-		panel.add(label);
+		JLabel lblTelefono = new JLabel("Telefono");
+		lblTelefono.setBounds(20, 104, 46, 14);
+		panel.add(lblTelefono);
 		
 		textField = new JTextField();
-		textField.setText("000-000-0000");
-		textField.setEditable(false);
 		textField.setColumns(10);
-		textField.setBounds(80, 101, 123, 20);
+		textField.setBounds(80, 101, 177, 20);
 		panel.add(textField);
 		
 		JLabel lblDireccion = new JLabel("Direcci\u00F3n");
@@ -111,18 +90,55 @@ public class Crear_Factura extends JDialog {
 		panel.add(lblEmail);
 		
 		textField_1 = new JTextField();
-		textField_1.setEditable(false);
 		textField_1.setColumns(10);
 		textField_1.setBounds(80, 235, 177, 20);
 		panel.add(textField_1);
 		
-		JLabel lblSelecionarComponentes = new JLabel("Componentes");
-		lblSelecionarComponentes.setBounds(20, 286, 149, 14);
+		JLabel lblSelecionarComponentes = new JLabel("Combos \r\ndisponibles");
+		lblSelecionarComponentes.setBounds(20, 278, 98, 26);
 		panel.add(lblSelecionarComponentes);
 		
-		JButton btnSeleccionar = new JButton("Seleccionar");
-		btnSeleccionar.setBounds(97, 282, 110, 23);
-		panel.add(btnSeleccionar);
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(80, 136, 327, 88);
+		panel.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(80, 67, 177, 20);
+		panel.add(textField_3);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(128, 281, 129, 20);
+		panel.add(comboBox);
+		
+		JLabel lblNewLabel = new JLabel("Total");
+		lblNewLabel.setBounds(20, 334, 46, 14);
+		panel.add(lblNewLabel);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(80, 331, 177, 20);
+		panel.add(textField_4);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(0, 410, 434, 37);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Cancelar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(335, 7, 89, 23);
+		panel_1.add(btnNewButton);
+		
+		JButton btnFacturar = new JButton("Facturar");
+		btnFacturar.setBounds(236, 7, 89, 23);
+		panel_1.add(btnFacturar);
 		
 		JLabel label_1 = new JLabel("Crear nueva factura:");
 		label_1.setVerticalAlignment(SwingConstants.BOTTOM);
